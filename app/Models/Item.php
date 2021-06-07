379415ef1000c459da3c6d77129c505fd736bb2b
Rafal Kadzevic <kadzewiczrafal@gmail.com>
2021-06-07 17:32:44 +0300

    First commit, made migrations, models, REST CRUD

---
+++ app/Models/Item.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'category_id',
+        'name',
+        'value',
+        'quality'
+    ];
+    public function category(){
+        return $this->belongsTo(Category::class)->withDefault();
+    }
+}
---
+++ database/migrations/2021_06_07_105814_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('name');
+            $table->float('value');
+            $table->integer('quality');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
